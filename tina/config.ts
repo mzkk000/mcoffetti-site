fa45439b84ca91ab368b2285904baff82f026686
mzkk000 <90523947+mzkk000@users.noreply.github.com>
2025-11-16 14:45:19 +0100

    Update config.ts

--- tina/config.ts
+++ tina/config.ts
@@ -1,9 +1,21 @@
 import { defineConfig } from "tinacms";
 import schema from "./schema";
 
+const branch =
+  process.env.NEXT_PUBLIC_TINA_BRANCH ||
+  process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF ||
+  process.env.HEAD ||
+  "main";
+
 export default defineConfig({
-  branch: process.env.NEXT_PUBLIC_TINA_BRANCH || "main",
+  branch,
+  // Tina Cloud credentials
   clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID!,
   token: process.env.TINA_TOKEN!,
+  // Required build config for Tina admin
+  build: {
+    publicFolder: "public",
+    outputFolder: "admin"
+  },
   schema
 });
